79e7dc2014e7b6b6cc85d6cb0813d06327c20464
rikcav <henriquearau000@gmail.com>
2024-10-19 06:09:40 -0300

    fix: fixed non-working swagger

--- backend/src/main/java/com/system/angels/AngelsApplication.java
+++ backend/src/main/java/com/system/angels/AngelsApplication.java
@@ -2,10 +2,8 @@ package com.system.angels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class AngelsApplication {
 
 	public static void main(String[] args) {
