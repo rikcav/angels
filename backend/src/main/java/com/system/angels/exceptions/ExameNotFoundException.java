e50b75884c097bd22973a65a15edea6d660b11c7
Camila Claudino <camila.claudinoo123@gmail.com>
2024-10-28 09:43:35 -0300

    fix: deleting Exame

--- backend/src/main/java/com/system/angels/exceptions/ExameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.system.angels.exceptions;
-
-public class ExameNotFoundException extends RuntimeException {
-    public ExameNotFoundException(String message) {
-        super(message);
-    }
-}
