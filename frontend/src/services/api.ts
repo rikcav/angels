7867d0d13f9f43948a855fc3a65439fb513c5a71
Camila Claudino <camila.claudinoo123@gmail.com>
2024-10-29 13:14:31 -0300

    fix: fixing merge conflicts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,3 @@ import axios, { AxiosInstance } from 'axios';
 export const api: AxiosInstance = axios.create({
   baseURL: 'http://localhost:8081'
 });
-
-export const apiIA: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8080'
-});
