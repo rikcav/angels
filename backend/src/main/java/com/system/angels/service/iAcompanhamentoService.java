85af7f2f4b87c90b1174f1efc5cfb3c749803e93
Camila Claudino <camila.claudinoo123@gmail.com>
2024-11-01 15:58:26 -0300

    fix: refatoração da entidade de encaminhamento

--- backend/src/main/java/com/system/angels/service/iAcompanhamentoService.java
+++ backend/src/main/java/com/system/angels/service/iAcompanhamentoService.java
@@ -1,19 +1,23 @@
 package com.system.angels.service;
 
 import com.system.angels.domain.Acompanhamento;
+import com.system.angels.dto.create.CadastrarAcompanhamentoDTO;
+import com.system.angels.dto.response.VisualizarAcompanhamentoDTO;
+
 import java.util.List;
 
 public interface iAcompanhamentoService {
-  List<Acompanhamento> listarAcompanhamentos();
-
-  Acompanhamento buscarAcompanhamentoPorId(Long id);
-
-  Acompanhamento registrarAcompanhamento(Acompanhamento acompanhamento);
-
-  void deletarAcompanhamento(Long id);
-
-  List<Acompanhamento> listarAcompanhamentoPorGestacaoId(Long gestacaoId);
-
-  Acompanhamento atualizarAcompanhamento(Long id, Acompanhamento acompanhamentoAtualizado);
-
+    List<Acompanhamento> listarAcompanhamentos();
+    
+    Acompanhamento buscarAcompanhamentoPorId(Long id);
+    
+    Acompanhamento registrarAcompanhamento(Acompanhamento acompanhamento);
+    
+    void deletarAcompanhamento(Long id);
+    
+    List<Acompanhamento> listarAcompanhamentoPorGestacaoId(Long gestacaoId);
+    
+    Acompanhamento atualizarAcompanhamento(Long id, Acompanhamento acompanhamentoAtualizado);
+    
+    VisualizarAcompanhamentoDTO cadastrarAcompanhamento(Long gestacaoId, CadastrarAcompanhamentoDTO cadastroAcompanhamentoDTO);
 }
