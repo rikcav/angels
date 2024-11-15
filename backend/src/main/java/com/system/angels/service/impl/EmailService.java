28134cbfa7e6b64b447d0661ff401a5127574316
rikcav <henriquearau000@gmail.com>
2024-11-15 13:59:14 -0300

    feat: added email service files

---
+++ backend/src/main/java/com/system/angels/service/impl/EmailService.java
@@ -0,0 +1,27 @@
+package com.system.angels.service.impl;
+
+import com.system.angels.service.iEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService implements iEmailService {
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+        System.out.println("Email sent to " + to);
+    }
+}
