d447d6dbb298817be79c6e8b2a328741b07e0e10
rikcav <henriquearau000@gmail.com>
2024-11-29 16:33:21 -0300

    feat: fixed dependencies and user to work with login

--- backend/src/main/java/com/system/angels/domain/User.java
+++ backend/src/main/java/com/system/angels/domain/User.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 
 @Entity
 @Table(name = "users")
@@ -19,29 +19,41 @@ public class User implements UserDetails {
     private String username;
     @Column(unique = true, nullable = false)
     private String password;
+    @Column
+    private String role = "USER";
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
