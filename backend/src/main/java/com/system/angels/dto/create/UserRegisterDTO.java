e39c15b6b4b6ae53caf6d9d5d15b07c3ca939a67
rikcav <henriquearau000@gmail.com>
2024-11-29 16:14:40 -0300

    feat: added user ROs and DTOs

---
+++ backend/src/main/java/com/system/angels/dto/create/UserRegisterDTO.java
@@ -0,0 +1,6 @@
+package com.system.angels.dto.create;
+
+public record UserRegisterDTO(
+        String username,
+        String password) {
+}
---
+++ backend/src/main/java/com/system/angels/dto/response/UserRO.java
@@ -0,0 +1,4 @@
+package com.system.angels.dto.response;
+
+public record UserRO(String username) {
+}
