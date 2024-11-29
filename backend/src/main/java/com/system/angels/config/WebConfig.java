b80a5a1433d9cde54110a4027e7f03d9221a422f
rikcav <henriquearau000@gmail.com>
2024-11-29 16:19:01 -0300

    feat: added bearer key to swagger

--- backend/src/main/java/com/system/angels/config/WebConfig.java
+++ backend/src/main/java/com/system/angels/config/WebConfig.java
@@ -1,14 +1,27 @@
 package com.system.angels.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER).name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
     }
 }
